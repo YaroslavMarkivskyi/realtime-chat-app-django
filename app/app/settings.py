59d25d492d71f14009a59f8d5a87e90800e0e1d8
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-15 22:28:41 +0300

    feat: Add CHANNEL_LAYERS configuration for chat functionality

--- app/app/settings.py
+++ app/app/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 ASGI_APPLICATION = 'app.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
